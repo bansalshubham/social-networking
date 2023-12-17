0fe15629430bc574e794ebf3b2cdb2eed8e7f0c9
bansalshubham <bansalshubham1510@gmail.com>
2023-12-17 12:18:43 +0530

    save

--- social_networking/backend_api/pagination.py
+++ social_networking/backend_api/pagination.py
@@ -1,5 +1,5 @@
 from rest_framework import status, pagination
 class UserSearchPagination(pagination.PageNumberPagination):
-    page_size = 3
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
